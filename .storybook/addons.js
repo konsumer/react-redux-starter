32b98f818d32931ee54a732d837d3fc8f70766ad
David Konsumer <konsumer@jetboystudio.com>
2016-12-02 20:07:09 -0800

    adding storybook.io comments hooks

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,4 @@
 import '@kadira/storybook/addons'
 import '@kadira/storybook-addon-knobs/register'
+import '@kadira/storybook-database-cloud/register'
+import '@kadira/storybook-addon-comments/register'
