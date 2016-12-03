1f632cd8c9c9633a5b06d7e41da174451cb5311a
David Konsumer <konsumer@jetboystudio.com>
2016-12-02 17:15:56 -0800

    nicer docstring

--- src/components/stories/Notification.js
+++ src/components/stories/Notification.js
@@ -4,14 +4,18 @@ import { text, boolean, select } from '@kadira/storybook-addon-knobs'
 
 import { Notification } from '../Notification'
 
+const docs = `
+This component handles toast notifications for "info", "success", and "error" type.
+`
+
 storiesOf('Notification', module)
-	.addWithInfo('simple usage', 'Basic usage of Notification', () => (
+	.addWithInfo('simple usage', docs, () => (
 	  <Notification
 	  	message={text('Message', 'Here is a message')}
 	    type={select('Type', ['info', 'success', 'error'], 'info')}
 	    hidden={boolean('Hidden', false)}
 	    stay={boolean('Stay', true)}
-	    onClose={ action('x-click')}
+	    onClose={ action('click', 'close')}
 	  />
 	))
 
